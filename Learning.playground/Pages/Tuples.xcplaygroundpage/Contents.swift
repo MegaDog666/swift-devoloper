267cf3bae036cb9ebe17794fda15ca2abe965536
MegaDog666 <Moonandre9999@mail.ru>
2023-09-09 08:44:04 +0300

    new lessons #2

---
+++ Learning.playground/Pages/Tuples.xcplaygroundpage/Contents.swift
@@ -0,0 +1,42 @@
+import Foundation
+
+
+ 
+var userName: String = "Hello"
+var userPremium: Bool = false
+var userIsNew: Bool = true
+func getUserName() -> String {
+    userName
+}
+func getUserPremium() -> Bool {
+    userPremium
+}
+
+// Ограничение 1 return
+func getUserInfo() -> String {
+    let name = getUserName()
+    let isPremium = getUserPremium()
+    return name
+}
+
+// Кортеж может обьединять несколько объектов данных
+func getUserInfo2() -> (String, Bool) {
+    let name = getUserName()
+    let isPremium = getUserPremium()
+    return (name, isPremium)
+}
+
+var userData1: String = userName
+var userData2: (String, Bool, Bool) = (userName, userPremium, userIsNew)
+
+let info1 = getUserInfo2()
+let name1 = info1.0
+
+func getUserInfo3() -> (name: String, isPremium: Bool) {
+    let name = getUserName()
+    let isPremium = getUserPremium()
+    return (name, isPremium)
+}
+
+let info2 = getUserInfo3()
+let name2 = info2.name
